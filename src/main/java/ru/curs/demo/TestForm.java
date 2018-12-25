f8e7816387192c97be1084862736a53b31045293
inponomarev <iponomarev@mail.ru>
2018-12-25 17:44:27 +0300

    initial commit

---
+++ src/main/java/ru/curs/demo/TestForm.java
@@ -0,0 +1,56 @@
+package ru.curs.demo;
+
+import ru.curs.celesta.CallContext;
+import ru.curs.celesta.dbutils.BasicCursor;
+import ru.curs.lyra.BasicGridForm;
+import ru.curs.lyra.LyraFormField;
+import ru.curs.lyra.LyraFormProperties;
+import ru.curs.lyra.LyraNamedElementHolder;
+
+public class TestForm extends BasicGridForm {
+
+    public TestForm(CallContext c){
+        super(c);
+        createAllBoundFields();
+    }
+
+    @Override
+    public int getGridHeight() {
+        return 0;
+    }
+
+    @Override
+    public String getSummaryRow() {
+        return null;
+    }
+
+    @Override
+    public BasicCursor _getCursor(CallContext callContext) {
+        return new OrderLineCursor(callContext);
+    }
+
+    @Override
+    public String _getId() {
+        return null;
+    }
+
+    @Override
+    public LyraFormProperties getFormProperties() {
+        return null;
+    }
+
+    @Override
+    protected LyraFormField _createUnboundField(LyraNamedElementHolder<LyraFormField> lyraNamedElementHolder, String s) {
+        return null;
+    }
+
+    @Override
+    protected void _createAllUnboundFields(LyraNamedElementHolder<LyraFormField> lyraNamedElementHolder) {
+
+    }
+
+    @Override
+    public void _beforeSending(BasicCursor basicCursor) {
+
+    }
+}
